6c5ee782da985fff2e43d8b91e9e0003f57e7b06
Floschy <69321390+flloschy@users.noreply.github.com>
2021-12-01 17:43:19 +0100

    Completed Day1

---
+++ 2021/Day1/a.py
@@ -0,0 +1,10 @@
+import time
+start = time.perf_counter()
+####
+last, count = 0, 0
+for line in open("./2021/Day1/input.txt"):
+    count += int(line.strip()) > last
+    last = int(line.strip())
+print(count-1)
+###
+print(f"Time Taken: {time.perf_counter()-start}ms")
---
+++ 2021/Day1/b.py
@@ -0,0 +1,10 @@
+import time
+start = time.perf_counter()
+###
+a, b, c, count  = 0, 0, 0, 0
+for line in open("./2021/Day1/input.txt"):
+    count += not (a == 0 or b == 0 or c == 0) and int(line.strip())+a+b > a+b+c
+    a, b, c = int(line.strip()), a, b
+print(count)
+###
+print(f"Time Taken: {time.perf_counter()-start}ms")
