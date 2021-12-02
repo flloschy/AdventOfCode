d85c8c6a97a19bc8cc9e1c93b433536461978f98
Floschy <69321390+flloschy@users.noreply.github.com>
2021-12-02 22:58:36 +0100

    Prepared Folders and Files

---
+++ 2021/Day19/b.py
@@ -0,0 +1,6 @@
+import time
+start = time.perf_counter()
+###
+
+###
+print(f"Time Taken: {time.perf_counter()-start}ms")
---
+++ prepare.py
@@ -0,0 +1,13 @@
+import os
+year = 2021
+
+if not os.path.exists(f"./{year}"):
+    os.mkdir(f"./{year}")
+for i in range(1, 26):
+    os.mkdir(f"./{year}/Day{i}")
+    open(f"./{year}/Day{i}/input.txt", "x")
+    with open(f"./{year}/Day{i}/a.py", "x") as afile:
+        afile.write("""import time\nstart = time.perf_counter()\n###\n\n###\nprint(f"Time Taken: {time.perf_counter()-start}ms")""")
+    with open(f"./{year}/Day{i}/b.py", "x") as afile:
+        afile.write("""import time\nstart = time.perf_counter()\n###\n\n###\nprint(f"Time Taken: {time.perf_counter()-start}ms")""")
+    open(f"./{year}/Day{i}/Challange.md", "x")
