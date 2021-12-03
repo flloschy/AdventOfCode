a2f7c9789530d14d8c823595ab668ca37c2693d3
Floschy <69321390+flloschy@users.noreply.github.com>
2021-12-03 19:46:59 +0100

    Day3 Complete

--- 2021/Day3/a.py
+++ 2021/Day3/a.py
@@ -1,6 +1,17 @@
 import time
 start = time.perf_counter()
 ###
+gammab = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]] # gamma bits
+for line in open("./2021/Day3/input.txt"):
+    for i, b in enumerate(line.strip()):
+        if b == "0": gammab[i][0] += 1
+        else: gammab[i][1] += 1
 
+ngammab, nepsilonb = "", "" # new gamma bits ||  new epsilon bits
+for b in gammab:
+    if b[0] > b[1]: ngammab += "1"; nepsilonb += "0"
+    else: ngammab += "0"; nepsilonb += "1"
+
+print(int(ngammab, 2)*int(nepsilonb, 2))
 ###
 print(f"Time Taken: {time.perf_counter()-start}ms")
--- 2021/Day3/b.py
+++ 2021/Day3/b.py
@@ -1,6 +1,37 @@
+# Original Code by -> Jonathan Paulson <-
+# Original Video -> https://www.youtube.com/watch?v=bFpsqFSCCsM <-
+# Original Github -> https://github.com/jonathanpaulson/AdventOfCode/blob/master/2021/3.py <-
+# I have NO idea how this days Challange works, so I just took this code /\/\/\ and edited a bit
+
 import time
 start = time.perf_counter()
 ###
+N = []
+for line in open("./2021/Day3/input.txt"): N.append(line.strip())
 
+for x in N:
+    assert len(x) == len(N[0])
+V = [[0 for _ in range(len(N[0]))] for _ in range(2)]
+for x in N:
+    for i, c in enumerate(x):
+        V[1 if c=='1' else 0][i] += 1
+gamma, epsilon = '', ''
+for i in range(len(N[0])):
+    if V[0][i] > V[1][i]: gamma += '0'; epsilon += '1'
+    else: gamma += '1'; epsilon += '0'
+
+A, B = list(N), list(N)
+for i in range(len(N[0])):
+    if len(A) > 1:
+        tempa, tempb = [x for x in A if x[i]=='0'], [x for x in A if x[i]=='1']
+        a0, a1 = len(tempa), len(tempb)
+        if a1 >= a0: A = tempb
+        else: A = tempa
+    if len(B) > 1:
+        tempa, tempb = [x for x in B if x[i]=='0'], [x for x in B if x[i]=='1']
+        b0, b1 = len(tempa), len(tempb)
+        if b1 >= b0: B = tempa
+        else: B = tempb
+print(int(A[0],2)*int(B[0],2))
 ###
 print(f"Time Taken: {time.perf_counter()-start}ms")
