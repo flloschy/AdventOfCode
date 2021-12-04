291aa0a376edb65688aabedf6fc7f183983aa64a
Floschy <69321390+flloschy@users.noreply.github.com>
2021-12-04 16:00:03 +0100

    Day4 Complete

--- 2021/Day4/a.py
+++ 2021/Day4/a.py
@@ -1,6 +1,70 @@
 import time
 start = time.perf_counter()
 ###
+filecontent = open("./2021/Day4/input.txt").read().split("\n")
+bingonumbers = filecontent[0].split(',')
+filecontent.remove(filecontent[0])
 
+########## set up board data
+boards = []
+tempboard = []
+for line in filecontent:
+    if line != '':
+        tempboard.append({
+            line.split()[0]: False,
+            line.split()[1]: False,
+            line.split()[2]: False,
+            line.split()[3]: False,
+            line.split()[4]: False
+        })
+    else:
+        boards.append(tempboard)
+        tempboard = []
+del tempboard
+##########
+
+winner = False
+finalscore = 0
+for num in bingonumbers:
+    if winner: break
+    for board in boards:
+        for n in board:
+            for key in n.keys():
+                if not n[key] and key == num:
+                    n[key] = True
+
+        horizontals = [[], [], [], [], []]
+        verticals = [[], [], [], [], []]
+        for i1, n in enumerate(board):
+            for i2, key in enumerate(n.keys()):
+                if n[key]:
+                    verticals[i1].append(None)
+                    horizontals[i2].append(None)
+
+        for current, vert in enumerate(verticals):
+            if len(vert) == 5:
+                winner = True
+                total = 0
+                for n in board:
+                    for key in n.keys():
+                        if not n[key]:
+                            total += int(key)
+                finalscore = total*int(num)
+                break
+
+        if winner: break
+
+        for current, hori in enumerate(horizontals):
+            if len(hori) == 5:
+                winner = True
+                total = 0
+                for n in board:
+                    for key in n.keys():
+                        if not n[key]:
+                            total += int(key)
+                finalscore = total*int(num)
+                break
+
+print(finalscore)
 ###
-print(f"Time Taken: {time.perf_counter()-start}ms")
+print(f"Time Taken: {time.perf_counter()-start}s")
