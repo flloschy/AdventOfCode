418eacedd9a6dbd1446befb0cd33a5a5557260f5
Floschy <69321390+flloschy@users.noreply.github.com>
2021-12-06 17:43:00 +0100

    Day 6 Complete

--- 2021/Day6/a.py
+++ 2021/Day6/a.py
@@ -1,6 +1,19 @@
-import time
+import time, sys
 start = time.perf_counter()
 ###
+fishlist = [int(i) for i in open("./2021/Day6/input.txt").read().split(',')]
+days = 80
+for day in range(1, days+1):
+    # Progress Line
+    # sys.stdout.write("\033[F") 
+    # sys.stdout.write("\033[K")
+    # print(f"[{'|'*int(((day//4)/(days//4))*100)}{'.'*(100-int(((day//4)/(days//4))*100))}] {day} / {days}")
+    for i in range(len(fishlist)):
+        fishlist[i] -= 1
+        if fishlist[i] == -1:
+            fishlist[i] = 6
+            fishlist.append(8)
 
+print(len(fishlist))
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
