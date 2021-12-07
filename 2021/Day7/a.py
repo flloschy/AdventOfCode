6d1d46e54ff59bb024da3cdede99bdac33384ad2
Floschy <69321390+flloschy@users.noreply.github.com>
2021-12-07 16:06:19 +0100

    Day 7 Complete

--- 2021/Day7/a.py
+++ 2021/Day7/a.py
@@ -1,6 +1,14 @@
 import time
 start = time.perf_counter()
 ###
-
+crabs = [int(x) for x in open("./2021/Day7/input.txt").read().split(',')]
+fuleneeds = []
+print(range(min(crabs), max(crabs)+1))
+for height in range(min(crabs), max(crabs)+1):
+    fule = 0
+    for crab in crabs:
+        fule += abs(height-crab)
+    fuleneeds.append(fule)
+print(min(fuleneeds))
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
--- 2021/Day7/b.py
+++ 2021/Day7/b.py
@@ -1,6 +1,16 @@
 import time
 start = time.perf_counter()
 ###
-
+crabs = [int(x) for x in open("./2021/Day7/input.txt").read().split(',')]
+fuleneeds = []
+for height in range(min(crabs), max(crabs)+1):
+    fule = 0
+    for crab in crabs:
+        lastfule = -1
+        for dif in range(abs(height-crab)+1):
+            fule += lastfule +1
+            lastfule += 1
+    fuleneeds.append(fule)
+print(min(fuleneeds))
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
