6805754d403a722c6113824868dbdbdcb606b9c0
Floschy <69321390+flloschy@users.noreply.github.com>
2021-12-09 17:27:15 +0100

    Day9 Complete

--- 2021/Day9/a.py
+++ 2021/Day9/a.py
@@ -1,6 +1,25 @@
 import time
 start = time.perf_counter()
 ###
-
+lines = []
+for x in open("./2021/Day9/input.txt").read().split('\n'):
+    li = []
+    for l in x: li.append(int(l))
+    lines.append(li)
+risk = 0
+for i, y in enumerate(lines):
+    for i2, x in enumerate(y):
+        adjacent = []
+        try: adjacent.append(lines[i-1][i2])
+        except: pass
+        try: adjacent.append(lines[i][i2-1])
+        except: pass
+        try: adjacent.append(lines[i][i2+1])
+        except: pass
+        try: adjacent.append(lines[i+1][i2])
+        except: pass
+        if x < min(adjacent):
+            risk += x +1
+print(risk)
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
--- 2021/Day9/b.py
+++ 2021/Day9/b.py
@@ -1,6 +1,41 @@
 import time
 start = time.perf_counter()
 ###
+lines = []
+for x in open("./2021/Day9/input.txt").read().split('\n'):
+    li = []
+    for l in x: li.append(int(l))
+    lines.append(li)
 
+basinbeginns = {}
+for i, y in enumerate(lines):
+    for i2, x in enumerate(y):
+        if x == 9: continue
+        tempx = i2
+        tempy = i
+        while True:
+            c1, c2, c3, c4 = False, False, False, False
+            try:
+                if tempx+1 < 100 and lines[tempy][tempx+1] < lines[tempy][tempx]: 
+                    c1 = True; tempx += 1
+            except: pass
+            try:
+                if tempx-1 > -1 and lines[tempy][tempx-1] < lines[tempy][tempx]: 
+                    c2 = True; tempx -= 1
+            except: pass
+            try:
+                if tempy+1 < 100 and lines[tempy+1][tempx] < lines[tempy][tempx]: 
+                    c3 = True; tempy += 1
+            except: pass
+            try:
+                if tempy-1 > -1 and lines[tempy-1][tempx] < lines[tempy][tempx]: 
+                    c4 = True; tempy -= 1
+            except: pass
+            if all([not c1, not c2, not c3, not c4]):
+                break
+        if (tempx, tempy) not in basinbeginns.keys(): basinbeginns.update({(tempx, tempy): 0})
+        basinbeginns[(tempx, tempy)] += 1
+basinbeginns = sorted(basinbeginns.values(), reverse=True)
+print(basinbeginns[0]*basinbeginns[1]*basinbeginns[2])
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
