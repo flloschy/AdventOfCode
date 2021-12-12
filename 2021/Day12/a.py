e3d9d082d58106a31616cf11b351b7150d9ae542
Floschy <69321390+flloschy@users.noreply.github.com>
2021-12-12 19:51:44 +0100

    Day12 Complete

--- 2021/Day12/a.py
+++ 2021/Day12/a.py
@@ -1,6 +1,18 @@
 import time
 start = time.perf_counter()
 ###
+caves = [line.replace("\n", "").split("-") for line in open("./2021/Day12/input.txt").readlines()]
 
+def loop(cur, visted, caves):
+    if cur == "end": return 1
+    routesfound = 0
+    for cave in caves:
+        if cave[0] == cur and not (cave[1].islower() and cave[1] in visted):
+            routesfound += loop(cave[1], visted + [cave[1]], caves)
+        elif cave[1] == cur and not (cave[0].islower() and cave[0] in visted):
+            routesfound += loop(cave[0], visted + [cave[0]], caves)
+    return routesfound
+
+print(loop("start", ["start"], caves))
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
--- 2021/Day12/b.py
+++ 2021/Day12/b.py
@@ -1,6 +1,26 @@
 import time
 start = time.perf_counter()
 ###
+caves = [line.replace("\n", "").split("-") for line in open("./2021/Day12/input.txt").readlines()]
 
+
+def loop(current, visited, caves, twice=False):
+    if current == "end": return 1
+    routesfound = 0
+    for cave in caves:
+        cave[0], cave[1] = cave
+        if cave[0] == current:
+            if not (cave[1].islower() and cave[1] in visited) :
+                routesfound += loop(cave[1], visited + [cave[1]], caves, twice)
+            elif not twice and cave[1] != "start":
+                routesfound += loop(cave[1], visited + [cave[1]], caves, True)
+        if cave[1] == current:
+            if not (cave[0].islower() and cave[0] in visited) :
+                routesfound += loop(cave[0], visited + [cave[0]], caves, twice)
+            elif not twice and cave[0] != "start":
+                routesfound += loop(cave[0], visited + [cave[0]], caves, True)
+    return routesfound
+
+print(loop("start", ["start"], caves))
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
