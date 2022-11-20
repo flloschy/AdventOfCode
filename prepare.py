d6793dc6804b705c2b82b0ce453e8215f405d34c
flloschy <floschy.email@gmail.com>
2022-11-20 14:38:06 +0100

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -1,5 +1,29 @@
 import os
-year = 2021
+year = 2022
+
+baseCode = """
+import time
+start = time.perf_counter()
+####
+content = open("<path>", "r").read()
+
+
+###
+print(f"Time Taken: {time.perf_counter()-start}s")
+"""
+
+baseMD = """
+# Day <day>
+### `- title -`
+#### Part 1
+> - [My](https://github.com/flloschy) answer was `- answer -` . ([Here](https://github.com/flloschy/AdventOfCode/blob/main/<year>/Day<day>/a.py))
+
+#### Part 2
+> - [My](https://github.com/flloschy) answer was `- answer -` . ([Here](https://github.com/flloschy/AdventOfCode/blob/main/<year>/Day<day>/b.py))
+
+###### [Source](https://adventofcode.com/<year>/day/<day>)
+"""
+
 
 if not os.path.exists(f"./{year}"):
     os.mkdir(f"./{year}")
@@ -7,7 +31,9 @@ for i in range(1, 26):
     os.mkdir(f"./{year}/Day{i}")
     open(f"./{year}/Day{i}/input.txt", "x")
     with open(f"./{year}/Day{i}/a.py", "x") as afile:
-        afile.write("""import time\nstart = time.perf_counter()\n###\n\n###\nprint(f"Time Taken: {time.perf_counter()-start}s")""")
+        afile.write(baseCode.replace("<path>", f"./{year}/Day{i}/input.txt"))
     with open(f"./{year}/Day{i}/b.py", "x") as afile:
-        afile.write("""import time\nstart = time.perf_counter()\n###\n\n###\nprint(f"Time Taken: {time.perf_counter()-start}s")""")
-    open(f"./{year}/Day{i}/Challange.md", "x")
+        afile.write(baseCode.replace("<path>", f"./{year}/Day{i}/input.txt"))
+    with open(f"./{year}/Day{i}/Challange.md", "x") as afile:
+        afile.write(baseMD.replace(
+            "<day>", f"{i}").replace("<year>", f"{year}"))
