27ee3171dcb3c22fcf8139079a8970649b0bdfbb
flloschy <floschy.email@gmail.com>
2022-12-01 14:45:44 +0100

    Day1

--- 2022/Day1/a.py
+++ 2022/Day1/a.py
@@ -3,7 +3,14 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day1/input.txt", "r").read()
-
+elfs = content.split("\n\n")
+for i, elf in enumerate(elfs):
+    calories = elf.split("\n")
+    total = 0
+    for calorie in calories:
+        total += int(calorie)
+    elfs[i] = total
+print(elfs[elfs.index(max(elfs))])
 
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
--- 2022/Day1/b.py
+++ 2022/Day1/b.py
@@ -3,7 +3,15 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day1/input.txt", "r").read()
-
+elfs = content.split("\n\n")
+for i, elf in enumerate(elfs):
+    calories = elf.split("\n")
+    total = 0
+    for calorie in calories:
+        total += int(calorie)
+    elfs[i] = total
+elfs.sort(reverse=True)
+print( elfs[0] + elfs[1] + elfs[2])
 
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
