b35299f3ca42c2ef1975c38b302e8f0d49e0f7e3
flloschy <floschy.email@gmail.com>
2022-12-02 15:44:10 +0100

    day2

--- 2022/Day2/a.py
+++ 2022/Day2/a.py
@@ -3,7 +3,34 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day2/input.txt", "r").read()
+rounds = content.splitlines()
+score = 0
 
+
+# A X = Rock | 1
+# B Y = Paper | 2
+# C Z = Scissors | 3
+# Loose | 0
+# Draw | 3
+# Win | 6
+
+points = {
+    'A X': 4,
+    'A Y': 8,
+    'A Z': 3,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 7,
+    'C Y': 2,
+    'C Z': 6
+}
+
+
+for round in rounds:
+    score += points[round]
+
+print(score)
 
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
--- 2022/Day2/b.py
+++ 2022/Day2/b.py
@@ -3,7 +3,34 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day2/input.txt", "r").read()
+rounds = content.splitlines()
+score = 0
 
+
+# X loose
+# Y draw
+# Z win
+# A = Rock | 1
+# B = Paper | 2
+# C = Scissors | 3
+
+points = {
+    'A X': 3,
+    'A Y': 4,
+    'A Z': 8,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 2,
+    'C Y': 6,
+    'C Z': 7
+}
+
+
+for round in rounds:
+    score += points[round]
+
+print(score)
 
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
