e1a5c826e4c7ae7bd346822a27c439243300802c
flloschy <floschy.email@gmail.com>
2022-12-04 20:35:01 +0100

    Day4

--- 2022/Day4/a.py
+++ 2022/Day4/a.py
@@ -3,7 +3,15 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day4/input.txt", "r").read()
-
-
+pairs = content.splitlines()
+count = 0
+for pair in pairs:
+    a, b = pair.split(",")
+    a, b = a.split("-"), b.split("-")
+    a1, a2, b1, b2 = int(a[0]), int(a[1]), int(b[0]), int(b[1])
+    if ((b1 <= a1 <= b2) and (b1 <= a2 <= b2)) or \
+       ((a1 <= b1 <= a2) and (a1 <= b2 <= a2)):
+        count += 1
+print(count)
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
--- 2022/Day4/b.py
+++ 2022/Day4/b.py
@@ -2,8 +2,20 @@
 import time
 start = time.perf_counter()
 ####
+
+
 content = open("./2022/Day4/input.txt", "r").read()
-
-
+pairs = content.splitlines()
+count = 0
+for pair in pairs:
+    a, b = pair.split(",")
+    a, b = a.split("-"), b.split("-")
+    a1, a2, b1, b2 = int(a[0]), int(a[1]), int(b[0]), int(b[1])
+    a = [x for x in range(a1, a2)]
+    b = range(b1, b2)
+    if (b1 <= a1 <= b2) or (b1 <= a2 <= b2) or \
+       (a1 <= b1 <= a2) or (a1 <= b2 <= a2):
+        count += 1
+print(count)
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
