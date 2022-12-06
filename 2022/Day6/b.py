bf77e193678d95460ad24e9a9ddd72b0d565f389
flloschy <floschy.email@gmail.com>
2022-12-06 17:37:07 +0100

    Day6

--- 2022/Day6/b.py
+++ 2022/Day6/b.py
@@ -4,6 +4,16 @@ start = time.perf_counter()
 ####
 content = open("./2022/Day6/input.txt", "r").read()
 
+buffer = [" "] * 14
+count = 0
+
+for char in list(content):
+    count += 1
+    buffer.pop(0)
+    buffer.append(char)
+    if len(set("".join(buffer).replace(" ", ""))) == 14:
+        break
+print(count)
 
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
