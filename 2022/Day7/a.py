a7182e4a3c6b99f34d654de5bb47336058d3248b
flloschy <floschy.email@gmail.com>
2022-12-07 21:28:26 +0100

    Day7

--- 2022/Day7/a.py
+++ 2022/Day7/a.py
@@ -3,7 +3,34 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day7/input.txt", "r").read()
-
+sizeSystem = {}
+currentDir = []
+for line in content.splitlines():
+    if line.startswith("$ cd"):
+        if line.endswith(".."):
+            currentDir.pop(-1)
+        else:
+            currentDir.append(line.split(" ")[-1])
+    elif line.startswith("$ ls"):
+        continue
+    else:
+        if line.startswith("dir "):
+            continue
+        else:
+            size, name = line.split(" ")
+            
+            for i, dir in enumerate(currentDir):
+                try:
+                    sizeSystem["/".join(currentDir[0:i] + [dir]).replace("//", "/")] += int(size)
+                except:
+                    sizeSystem["/".join(currentDir[0:i] + [dir]).replace("//", "/")] = int(size)
+totalSize = 0
+dirs = []
+for dir in sizeSystem.keys():
+    size = sizeSystem[dir]
+    if size <= 100000:
+        dirs.append(size)
+print(sum(dirs))
 
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
--- 2022/Day7/b.py
+++ 2022/Day7/b.py
@@ -3,6 +3,37 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day7/input.txt", "r").read()
+sizeSystem = {}
+currentDir = []
+for line in content.splitlines():
+    if line.startswith("$ cd"):
+        if line.endswith(".."):
+            currentDir.pop(-1)
+        else:
+            currentDir.append(line.split(" ")[-1])
+    elif line.startswith("$ ls"):
+        continue
+    else:
+        if line.startswith("dir "):
+            continue
+        else:
+            size, name = line.split(" ")
+            
+            for i, dir in enumerate(currentDir):
+                try:
+                    sizeSystem["/".join(currentDir[0:i] + [dir]).replace("//", "/")] += int(size)
+                except:
+                    sizeSystem["/".join(currentDir[0:i] + [dir]).replace("//", "/")] = int(size)
+
+required = 30000000 - (70000000 - sizeSystem['/'])
+usable = []
+for dir in sorted(sizeSystem, key=sizeSystem.get):
+    size = sizeSystem[dir]
+    if size >= required:
+        usable.append(size)
+
+print(min(usable))
+
 
 
 ###
