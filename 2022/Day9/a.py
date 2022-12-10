c3a111a30bb3306d9f96d13fd26951fa7bc39fd2
flloschy <floschy.email@gmail.com>
2022-12-10 17:03:55 +0100

    Day9

--- 2022/Day9/a.py
+++ 2022/Day9/a.py
@@ -4,6 +4,8 @@ start = time.perf_counter()
 ####
 content = open("./2022/Day9/input.txt", "r").read()
 
+# Me to stupid to understand
+# Used https://github.com/savbell/2022-Advent-of-Code/blob/master/day-09.py
 
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
