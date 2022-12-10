70511bc0e82d8242402e8aba737d01a23d377220
flloschy <floschy.email@gmail.com>
2022-12-10 16:48:35 +0100

    Day8

--- 2022/Day8/a.py
+++ 2022/Day8/a.py
@@ -3,7 +3,67 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day8/input.txt", "r").read()
+height = len(content.splitlines())
+width = len(list(content.splitlines()[0]))
+rows = [[[int(three), x, y] for x, three in enumerate(row)] for y, row in enumerate(content.splitlines())]
+columns = [[[rows[x][y][0], y, x] for x in range(width)] for y in range(height)]
 
+visible = [[False for _ in range(width)] for _ in range(height)]
+
+for row in rows + rows[::-1]:
+    highest = -1
+    for three in row:
+        h, x, y = three
+        if x == 0 or y == 0 or x == width-1 or y == height-1:
+            highest = h
+            visible[y][x] = True
+            continue
+        if highest < h:
+            highest = h
+            visible[y][x] = True
+    highest = -1
+    for three in row[::-1]:
+        h, x, y = three
+        if x == 0 or y == 0 or x == width-1 or y == height-1:
+            highest = h
+            visible[y][x] = True
+            continue
+        if highest < h:
+            highest = h
+            visible[y][x] = True
+
+for col in columns + columns[::-1]:
+    highest = -1
+    for three in col:
+        h, x, y = three
+        if x == 0 or y == 0 or x == width-1 or y == height-1:
+            highest = h
+            visible[y][x] = True
+            continue
+        if highest < h:
+            highest = h
+            visible[y][x] = True
+    highest = -1
+    for three in col[::-1]:
+        h, x, y = three
+        if x == 0 or y == 0 or x == width-1 or y == height-1:
+            highest = h
+            visible[y][x] = True
+            continue
+        if highest < h:
+            highest = h
+            visible[y][x] = True
+
+
+
+        
+count = 0
+for row in visible:
+    for three in row:
+        if three:
+            count += 1
+print(count)
+print(count == 1877)
 
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
--- 2022/Day8/b.py
+++ 2022/Day8/b.py
@@ -3,7 +3,55 @@ import time
 start = time.perf_counter()
 ####
 content = open("./2022/Day8/input.txt", "r").read()
+height = len(content.splitlines())
+width = len(list(content.splitlines()[0]))
+rows = [[[int(three), x, y] for x, three in enumerate(row)] for y, row in enumerate(content.splitlines())]
+score = [[0 for _ in range(width)] for _ in range(height)]
 
+def getScore(three):
+    h, x, y = three
+    up = 0
+    down = 0
+    left = 0
+    right = 0
+    # down
+    for y2 in range(y+1, height):
+        t = rows[y2][x][0]
+        if t < h:
+            down += 1
+        elif t == h or h < t:
+            down += 1
+            break
+    # up
+    for y2 in range(y-1, -1, -1):
+        t = rows[y2][x][0]
+        if t < h:
+            up += 1
+        elif t == h or h < t:
+            up += 1
+            break
+    # right
+    for x2 in range(x+1, width):
+        t = rows[y][x2][0]
+        if t < h:
+            right += 1
+        elif t == h or h < t:
+            right += 1
+            break
+    # left
+    for x2 in range(x-1, -1, -1):
+        t = rows[y][x2][0]
+        if t < h:
+            left += 1
+        elif t == h or h < t:
+            left += 1
+            break
+    return up * down * left * right
 
+scores = []
+for row in rows:
+    for three in row:
+        scores.append(getScore(three))
+print(max(scores))
 ###
 print(f"Time Taken: {time.perf_counter()-start}s")
