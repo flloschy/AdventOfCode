e3f305c27d902e3f33d84c0e9e2aa59f40da4bbd
flloschy <floschy.email@gmail.com>
2023-12-09 10:37:36 +0100

    Day 9

---
+++ 2023/Day9/b.py
@@ -0,0 +1,35 @@
+
+import time
+start = time.perf_counter()
+####
+content = open("./2023/Day9/input.txt", "r").read().splitlines()
+content = [[[int(num) for num in line.split(" ")]] for line in content]
+
+skip = []
+while True:
+    for i, values in enumerate(content):
+        if i in skip: continue
+        original, latest = values[0], values[-1]
+        values.append([latest[i+1] - last for i, last in enumerate(latest[:-1])])
+        
+        if all(value == 0 for value in values[-1]):
+            skip.append(i)
+
+    if len(skip) == len(content): break
+
+
+
+
+LastValues = []
+
+for line in content:
+    line.reverse()
+    for i, diffs in enumerate(line[:-1]):
+        line[i+1].insert(0, line[i+1][0] - diffs[0])
+    LastValues.append(line[-1][0])
+    
+print(sum(LastValues))
+
+
+###
+print(f"Time Taken: {time.perf_counter()-start}s")
